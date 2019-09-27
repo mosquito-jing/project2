1ad05087845d14f158debafbc7dc87290a7db861
mosquito-jing <81361424@qq,com>
2019-09-27 17:09:36 +0800

    math

---
+++ math/math.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 17:07:04 2019
+
+@author: Administrator
+"""
+def add(x,y):
+    z=x+y
+    return z
+def subs(x,y):
+    z=x-y
+    return z
+def mul(x,y):
+    z=x*y
+    return z
+def div(x,y):
+    z=x/y
+    return z
